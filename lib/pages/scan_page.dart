b36ea52fa18e3baa2844707dcfa8a3f1119164a8
Arbaniasfi18 <arbanidalimunthe@gmail.com>
2024-08-12 15:41:00 +0700

    feat: implement camera on page

--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:sleepy/partials/notification.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -10,7 +12,42 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   String textButton = "OFF";
   Alignment align = Alignment.centerLeft;
+  late CameraController camCon;
+  bool loading = true;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    init();
+  }
+
+  @override
+  void dispose() {
+    camCon.dispose();
+    super.dispose();
+  }
+
+  init() async {
+    List<CameraDescription> cameras = await availableCameras();
+    camCon = CameraController(cameras[1], ResolutionPreset.high);
+    camCon.initialize().then((value) {
+      setState(() {
+        loading = false;
+      });
+    }).catchError((error) {
+      if (error is CameraException) {
+        switch (error.code) {
+          case 'CameraAccessDenied':
+            cameraAccessNotifDeclined(context);
+            break;
+          default:
+            cameraAccessNotifDeclined(context);
+            break;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +71,25 @@ class _ScanPageState extends State<ScanPage> {
             const SizedBox(
               height: 36,
             ),
-            Container(
-              height: 452,
-              decoration: BoxDecoration(
-                  color: Colors.pink, borderRadius: BorderRadius.circular(20)),
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+                bottomLeft: Radius.circular(25),
+              ),
+              child: AspectRatio(
+                  aspectRatio: 2 / 3,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: loading
+                        ? const Center(
+                            child:
+                                CircularProgressIndicator(color: Colors.green))
+                        : CameraPreview(camCon),
+                  )),
             ),
             const SizedBox(
               height: 28,
@@ -56,26 +108,25 @@ class _ScanPageState extends State<ScanPage> {
                   width: 50,
                   height: 50,
                   child: ElevatedButton(
-                    onPressed: () {
-                      if (textButton == "OFF") {
-                        setState(() {
-                          textButton = "ON";
-                          align = Alignment.centerRight;
-                        });
-                      }else {
-                        setState(() {
-                          textButton = "OFF";
-                          align = Alignment.centerLeft;
-                        });
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      elevation: 3,
-                      backgroundColor: Colors.white,
-                      padding: EdgeInsets.zero,
-                    ),
-                    child: Text(textButton)
-                  ),
+                      onPressed: () {
+                        if (textButton == "OFF") {
+                          setState(() {
+                            textButton = "ON";
+                            align = Alignment.centerRight;
+                          });
+                        } else {
+                          setState(() {
+                            textButton = "OFF";
+                            align = Alignment.centerLeft;
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 3,
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Text(textButton)),
                 ),
               ),
             )
