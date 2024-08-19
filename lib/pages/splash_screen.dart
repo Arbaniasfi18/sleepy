59a632118066a57d809a5ca23fa19986dc2c7c0a
Arbaniasfi18 <arbanidalimunthe@gmail.com>
2024-08-20 01:55:33 +0700

    feat: add logo icon to splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:sleepy/model/push_notification.dart';
 import 'package:sleepy/pages/scan_page.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,9 +13,9 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   double circleSize = 10;
   Color color = Colors.blue;
+  bool? permission;
 
   late Timer timer;
   late Timer timer2;
@@ -22,17 +24,8 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    timer2 = Timer(const Duration(milliseconds: 500), () async {
-      setState(() {
-        circleSize = 258;
-        color = Colors.pink;
-      });
-    });
-    timer = Timer(const Duration(seconds: 5), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ScanPage()));
-    });
+    init();
   }
-  
 
   @override
   void dispose() {
@@ -40,24 +33,41 @@ class _SplashScreenState extends State<SplashScreen> {
     super.dispose();
   }
 
+  init() async {
+    notifRequest();
+    timerFunc();
+    print(permission);
+  }
+
+  void notifRequest() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  }
+
+  void timerFunc() {
+    timer2 = Timer(const Duration(milliseconds: 500), () async {
+      setState(() {
+        circleSize = 258;
+        color = Colors.pink;
+      });
+    });
+    timer = Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const ScanPage()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: AnimatedContainer(
+          child: Center(
+        child: AnimatedContainer(
             duration: const Duration(seconds: 1),
             curve: Curves.bounceOut,
             width: circleSize,
             height: circleSize,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(1000),
-            )
-          ),
-        )
-      ),
+            child: Image.asset("assets/images/logo.png")),
+      )),
     );
   }
 }
