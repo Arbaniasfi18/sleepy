71224eeb6c3ad06490ad96eb21e72b14c54c542d
Arbaniasfi18 <arbanidalimunthe@gmail.com>
2024-08-21 18:35:16 +0700

    feat: add camera controller function

--- lib/model/camera.dart
+++ lib/model/camera.dart
@@ -6,29 +6,49 @@ import 'package:sleepy/partials/notification.dart';
 class Camera {
 
   static late CameraController camCon;
-  
-  static Future<void> init(BuildContext context) async {
+  static CameraImage? camImg;
+
+
+  static Future<void> init() async {
 
     List<CameraDescription> cameraDesc = await availableCameras();
 
     camCon = CameraController(cameraDesc[0], ResolutionPreset.high);
 
-    camCon.initialize().then((value) {
-
-    }).catchError((err) {
-       if (err is CameraException) {
-        switch (err.code) {
-          case 'CameraAccessDenied':
-            cameraAccessNotifDeclined(context);
-            break;
-          default:
-            cameraAccessNotifDeclined(context);
-            break;
-        }
-      }
-    });
-
   }
 
+  static Future<void> camStart(BuildContext context, {
+    required Function loading,
+  }) async {
+
+    if (camCon.value.isPreviewPaused) {
+      camCon.resumePreview();
+    }else {
+      await camCon.initialize().then((value) {
+        loading(false);
+      }).catchError((err) {
+         if (err is CameraException) {
+          switch (err.code) {
+            case 'CameraAccessDenied':
+              cameraAccessNotifDeclined(context);
+              break;
+            default:
+              cameraAccessNotifDeclined(context);
+              break;
+          }
+        }
+      });
+    }
+  }
+  
+  static void camStop({
+    required Function setStateCallback,
+  }) {
+    setStateCallback(camCon.pausePreview());
+  }
+
+  static void camConDispose() {
+    camCon.dispose();
+  }
 
 }
